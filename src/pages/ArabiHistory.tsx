7ea042d22356079f6eb0d08836ce65a68628fb99
akhiljo88 <akhiljose060@gmail.com>
2025-07-17 14:06:25 +0530

    Updated ArabiHistory.tsx

--- src/pages/ArabiHistory.tsx
+++ src/pages/ArabiHistory.tsx
@@ -275,7 +275,7 @@ const ArabiHistory: React.FC = () => {
               <p className="text-gray-700 leading-relaxed text-lg font-serif text-center">
                 The first school in Arabi was established in 1970 as a result of the collective efforts of a people. Half a century later, this school still plays a major role in the socio-cultural growth of Arabi. As we walk the historical path of today's Arabi, who are advancing on the path of development, we are amazed by the history of a people who conquered mountains and hills through hard work and unity.
               </p>
-
+              \n
               <p className="text-gray-800 leading-relaxed mb-6 text-lg font-serif text-center">by Amal Jolly
               </p>
             
